0d0567f96160649455e678d1574403df739cbda8
Temidayo Uji <solomond6@yahoo.com>
2018-11-09 10:34:21 +0100

    Created the migration and model classes for users and contacts

---
+++ server/migrations/20181108133049-create-contacts.js
@@ -0,0 +1,44 @@
+module.exports = {
+    up: (queryInterface, Sequelize) =>
+        queryInterface.createTable('Contacts', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            title: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            firstname: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            lastname: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            phone_number: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            address: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        }),
+    down: (queryInterface /* , Sequelize */ ) => queryInterface.dropTable('Contacts'),
+};
---
+++ server/models/contacts.js
@@ -0,0 +1,69 @@
+module.exports = (sequelize, DataTypes) => {
+    const Contacts = sequelize.define("Contacts", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            validate: {
+                isUnique: function(value, next) {
+                    Contacts.find({
+                            where: { email: value },
+                            attributes: ["id"]
+                        })
+                        .done(function(error, user) {
+                            if (error)
+                            // Some unexpected error occured with the find method.
+                                return next(error);
+                            if (user)
+                            // We found a user with this email address.
+                            // Pass the error to the next method.
+                                return next("Email address already in use!");
+                            // If we got this far, the email address hasn't been used yet.
+                            // Call next with no arguments when validation is successful.
+                            next();
+                        });
+                }
+            }
+        },
+        phone_number: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isUnique: function(value, next) {
+                    Contacts.find({
+                            where: { phone_number: value },
+                            attributes: ["id"]
+                        })
+                        .done(function(error, user) {
+                            if (error)
+                            // Some unexpected error occured with the find method.
+                                return next(error);
+                            if (user)
+                            // We found a user with this email address.
+                            // Pass the error to the next method.
+                                return next("Phone Number already in use!");
+                            // If we got this far, the email address hasn't been used yet.
+                            // Call next with no arguments when validation is successful.
+                            next();
+                        });
+                }
+            }
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    });
+
+    return Contacts;
+};
